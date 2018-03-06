d4c2754ecd398b988e5d97590ef885cb26ae328c
diegoruiz <diego_paul16@icloud.com>
2018-03-06 18:16:12 +0100

    ajout de la class Barber et BarberManager

---
+++ src/domaine/Barber.java
@@ -0,0 +1,7 @@
+package domaine;
+
+/**
+ * Created by diegoruiz on 06.03.18.
+ */
+public class Barber {
+}
---
+++ src/manager/BarberManager.java
@@ -0,0 +1,7 @@
+package manager;
+
+/**
+ * Created by diegoruiz on 06.03.18.
+ */
+public class BarberManager {
+}
