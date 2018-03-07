ec5f09a346299aba41151fe1a9dc8186e4f81c78
jerminiplap <jerministephane@gmail.com>
2018-03-07 21:15:01 +0100

    completé la class personne et instanciation de celle-ci

--- src/domaine/Persone.java
+++ src/domaine/Persone.java
@@ -5,4 +5,15 @@ package domaine;
  */
 public class Persone {
 
+	private int no;
+    private String nom;
+    
+    public Persone(int no, String nom) { this.no=no; this.nom=nom; }
+    public Persone(int no) { this(no, ""); }
+    
+    public int getNo() { return no; }
+    public String getNom() { return nom; }
+    
+    public boolean equals(Object obj) { return this.no == ((Persone)obj).no; }
+    public String toString() { return nom; }
 }
---
+++ src/domaine/Persone.java.bak
@@ -0,0 +1,8 @@
+package domaine;
+
+/**
+ * Created by diegoruiz on 04.03.18.
+ */
+public class Persone {
+
+}
