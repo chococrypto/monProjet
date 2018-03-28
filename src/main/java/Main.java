17ffb0756f39618561873bb4e564d41219cbf5e9
diegoruiz <diego_paul16@icloud.com>
2018-03-29 01:15:19 +0200

    le répertoire lib (src/lib) a été ajouter  pour ajouter nous jar. Le fichier POM a été modifier

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import com.google.gson.GsonBuilder;
 import domaine.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import outils.Validation;
+
 
 import java.util.Date;
 
@@ -14,12 +16,17 @@ public class Main {
     public static void main(String[] args) {
 
         Customer customer = new Customer(1,"tot@hotmail.com","09876534567","toto","tata", new Date());
-        GsonBuilder builder = new GsonBuilder();
+        /*GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        System.out.println(gson.toJson(customer));
+        System.out.println(gson.toJson(customer));*/
+        System.out.println(Validation.isDoubleValid("1"));
+       /*Logger logger = LoggerFactory.getLogger(Main.class);
+       logger.info("Ecrire quelque chose d'utile");*/
 
-         Logger logger = LoggerFactory.getLogger(Customer.class);
-        logger.info("Ecrire quelque chose d'utile");
+        //faire : géneration un fichier manif qui point vers le fichier Main.java
+        // creer des jar à partir des point class puis l'intsaller en local
+        //configuer le builder --> pom.xml -> Manifest.MF
+        //Creer un uberJar dans un Profil (Pom)
 
     }
 }
