ed50f1e011a6e4e59faa704ff667eb750318c0cd
diegoruiz <diego_paul16@icloud.com>
2018-04-21 22:00:37 +0200

    modification du pom & création des test unitaire avec l'outil testNG

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,32 @@
+import ch.monProjet.domaine.Customer;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+
+/**
+ * Created by diegoruiz on 20.04.18.
+ */
+public class CustomerTest {
+
+    @Test
+    public void compareToEquals(){
+        Customer customer1 = new Customer(1,"tot@hotmail.com","09876534567","toto","tata", new Date());
+        Customer customer2 = new Customer(1,"tot@hotmail.com","09876534567","toto","tata", new Date());
+        Assert.assertEquals(customer1.compareTo(customer2),0);
+    }
+    @Test
+    public void compareToPusGrandBA(){
+        Customer a = new Customer(1,"qot@hotmail.com","09876534567","toto","tata", new Date());
+        Customer b = new Customer(2,"zot@hotmail.com","09876534567","zoto","diego", new Date());
+        Assert.assertTrue(a.compareTo(b) > 0);
+    }
+
+    @Test
+    public void compareToPusGrandAB(){
+        Customer a = new Customer(1,"qot@hotmail.com","09876534567","toto","anax", new Date());
+        Customer b = new Customer(2,"zot@hotmail.com","09876534567","zoto","diego", new Date());
+        Assert.assertTrue(a.compareTo(b) < 0);
+    }
+
+}
