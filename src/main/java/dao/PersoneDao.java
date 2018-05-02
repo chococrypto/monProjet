cf016fd28ab4f60b1eda882c7c4edf5b7f0cbffc
diegoruiz <diego_paul16@icloud.com>
2018-05-03 00:43:38 +0200

    ajout du package dao avec une class et  d'un fichier data

---
+++ src/main/java/dao/PersoneDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import domaine.Persone;
+
+import java.util.ArrayList;
+
+/**
+ * Created by diegoruiz on 02.05.18.
+ */
+public class PersoneDao {
+
+    public ArrayList<Persone> datas(){
+        ArrayList<Persone> lst = new ArrayList<>();
+
+
+        return lst;
+    }
+}
