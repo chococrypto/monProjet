2f73ab5a7618358db90cc3ca7f02309ef91f76b6
diegoruiz <diego_paul16@icloud.com>
2018-05-04 23:04:41 +0200

    suppresion du code inutile

--- src/main/java/ch/heg/monProjet/Main.java
+++ src/main/java/ch/heg/monProjet/Main.java
@@ -15,19 +15,5 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Hello");
-        System.out.println("===========================");
-        Customer customer = new Customer(1,"tot@hotmail.com","09876534567","toto","tata", new Date());
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        System.out.println(gson.toJson(customer));
-        System.out.println(customer);
-        System.out.println("============= 2 =============");
-        System.out.println(customer.toJson());
-        System.out.println("============= 3 =============");
-        PersoneDao dao = new PersoneDao(new Reader());
-
-       List<Persone> lst = dao.datas();
-        System.out.println(lst.size());
     }
 }
