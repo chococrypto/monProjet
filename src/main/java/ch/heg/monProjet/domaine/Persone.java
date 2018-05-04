b7591cf9b60e6ec2ffdab7c2dc9117420ab32ca2
diegoruiz <diego_paul16@icloud.com>
2018-05-04 21:46:48 +0200

    suppresion des getter et setter qui ne sont pas utiliser

--- src/main/java/ch/heg/monProjet/domaine/Persone.java
+++ src/main/java/ch/heg/monProjet/domaine/Persone.java
@@ -26,20 +26,6 @@ abstract public class Persone implements Comparable{
     public Persone(int num){
         this(num,"","","","",new Date());
     }
-    //# Accessor
-    //getter
-    public int getNum() {return num;}
-    public String getEmail() {return email;}
-    public String getPhone() {return phone;}
-    public String getFirstName() {return firstName;}
-    public String getLastName() {return lastName;}
-    public Date getBirthday() {return birthday;}
-    //setter
-    public void setEmail(String email) {this.email = email;}
-    public void setPhone(String phone) {this.phone = phone;}
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public void setLastName(String lastName) {this.lastName = lastName;}
-    public void setBirthday(Date birthday) {this.birthday = birthday;}
 
     //# Comparison
     //Equals
--- src/test/java/ch/heg/monProjet/domaine/PersonneTest.java
+++ src/test/java/ch/heg/monProjet/domaine/PersonneTest.java
@@ -1,11 +1,7 @@
 package ch.heg.monProjet.domaine;
 
-import ch.heg.monProjet.domaine.Customer;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.util.Date;
 
 
